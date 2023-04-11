35c812a655a5e6f316d505392288e531d7934688
nratliff2078 <72581364+nratliff2078@users.noreply.github.com>
2023-04-11 16:48:01 -0400

    Minor fixes

--- DinkPinkTruck/scenes/player_gun.gd
+++ DinkPinkTruck/scenes/player_gun.gd
@@ -1,20 +1,24 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 export var angle = 90
 var bullet_speed = 100
 onready var bullet = preload("res://scenes/player_bullet.tscn")
-# Called when the node enters the scene tree for the first time.
+
 #func _ready():
 #	print("Viewport Resolution is: ", get_viewport_rect().size)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	# This is how we do gun angle based upon mouse
+	
+	 #var player_pos = get_tree().get_root().get_node("world_root/main_player/player").global_position
+	 #var mouse_pos = get_viewport().get_mouse_position()
+	 #var x_diff = mouse_pos[0] - player_pos[0]
+	 #var y_diff = mouse_pos[1] - player_pos[1]
+	 #var angle = atan2(-y_diff,x_diff)
+	 #self.global_rotation_degrees = rad2deg(angle)
+	
 	if Input.is_action_pressed("gun_up"):
 		angle -= 0.1
 	
@@ -23,16 +27,15 @@ func _process(delta):
 		
 	var deg_angle = rad2deg(angle)
 
-	#print("Angle:", deg_angle)
 	self.global_rotation_degrees = deg_angle
 
-	var gun_angle = angle
-	
-	
 	if Input.is_action_pressed("space"):
 		var bullet_instance = bullet.instance()
 		bullet_instance.position = get_tree().get_root().get_node("world_root/main_player/player").global_position
-		bullet_instance.rotation_degrees = gun_angle
-		bullet_instance.apply_impulse(Vector2(), Vector2(-bullet_speed, 0))
+		
+		bullet_instance.rotation_degrees = angle
+		bullet_instance.global_rotation_degrees = deg_angle
+		
+		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed,0))
 		get_tree().get_root().add_child(bullet_instance)
 		
